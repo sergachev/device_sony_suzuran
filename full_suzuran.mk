5d15797a68f56140d456fadab7b54f05e84639ae
Zane <zanehunt@me.com>
2017-03-01 16:57:38 +1300

    Initial Z5C bringup on top of Z5 trees

--- full_suzuran.mk
+++ full_suzuran.mk
@@ -19,11 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from sumire device
-$(call inherit-product, device/sony/sumire/device.mk)
+$(call inherit-product, device/sony/suzuran/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_BRAND := Sony
-PRODUCT_NAME := full_sumire
+PRODUCT_NAME := full_suzuran
 PRODUCT_MANUFACTURER := Sony
-PRODUCT_DEVICE := sumire
-PRODUCT_MODEL := Xperia Z5
+PRODUCT_DEVICE := suzuran
+PRODUCT_MODEL := Xperia Z5 Compact
