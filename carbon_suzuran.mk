2371e78ba40d2f8055a4e9a3b461fffa80da0454
Zane <nzzane_xda@icloud.com>
2017-09-06 10:22:56 +1200

    Carbonify

--- carbon_suzuran.mk
+++ carbon_suzuran.mk
@@ -16,10 +16,13 @@
 
 $(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
-PRODUCT_NAME := lineage_suzuran
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+PRODUCT_NAME := carbon_suzuran
 
 # Use the latest approved GMS identifiers
 
